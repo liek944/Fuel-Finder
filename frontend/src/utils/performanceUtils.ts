1653ba32800aa5d6d9156a0d3054dada46b4934c
liek944 <185925440+liek944@users.noreply.github.com>
2025-10-13 09:23:55 +0800

    fix build error

--- frontend/src/utils/performanceUtils.ts
+++ frontend/src/utils/performanceUtils.ts
@@ -31,11 +31,10 @@ export function throttle<T extends (...args: any[]) => any>(
   limit: number
 ): (...args: Parameters<T>) => void {
   let inThrottle: boolean = false;
-  let lastResult: ReturnType<T>;
 
   return function (this: any, ...args: Parameters<T>): void {
     if (!inThrottle) {
-      lastResult = func.apply(this, args);
+      func.apply(this, args);
       inThrottle = true;
       setTimeout(() => {
         inThrottle = false;
